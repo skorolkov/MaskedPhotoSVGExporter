5981eef9bca0b4c67e7e7020b3e9df2055f9f040
Sergey Korolkov <skorolkov@ipolecat.com>
2016-10-19 18:18:00 +0300

    zooming image in scroll view

--- MaskedPhotoSVGExporter/MaskPhotoController.m
+++ MaskedPhotoSVGExporter/MaskPhotoController.m
@@ -29,7 +29,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupImageView];
+    [self setupImageInScrollView];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    CGFloat minimumZoomScale = 1;
+    if (self.image.size.width < self.image.size.height) {
+        minimumZoomScale = self.scrollView.bounds.size.width / self.image.size.width;
+    } else {
+        minimumZoomScale = self.scrollView.bounds.size.height / self.image.size.height;
+    }
+    
+    if (self.scrollView.minimumZoomScale != minimumZoomScale) {
+        self.scrollView.minimumZoomScale = minimumZoomScale;
+        self.scrollView.zoomScale = minimumZoomScale;
+    }
 }
 
 #pragma mark UIScrollViewDelegate
@@ -40,8 +56,9 @@
 
 #pragma mark Views Support
 
-- (void)setupImageView {
+- (void)setupImageInScrollView {
     self.imageView.image = self.image;
+    self.scrollView.maximumZoomScale = 1;
 }
 
 @end
